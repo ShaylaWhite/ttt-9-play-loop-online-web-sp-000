7bfd3c9ad2bc5f334a1cc435575286285043b4c9
Shayla White <shayladwhite1@gmail.com>
2019-04-22 04:48:08 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,3 +36,14 @@ def turn(board)
 end
 
 # Define your play method below
+
+def play (board)
+ loop do 
+  counter += 1
+  puts "Iteration #{counter} of the loop"
+  if counter >= 10 
+    break
+  end
+end
+end
+  
